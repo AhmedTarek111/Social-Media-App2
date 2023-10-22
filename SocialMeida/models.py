5ed75c85c443ed38531ee8c7383d6f51cd4f9af3
AhmedTarek2000 <ahmedtarekalsaudi@gmail.com>
2023-10-23 01:12:06 +0300

    design the profile model and added requirements file

--- SocialMeida/models.py
+++ SocialMeida/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE,related_name='user_profile')
+    profile_image = models.ImageField(upload_to='profile-user-image',default='user_default.png')
+    bio= models.TextField(max_length=400,null=True,blank=True)
+    location = models.CharField(max_length=200,null=True,blank=True)
